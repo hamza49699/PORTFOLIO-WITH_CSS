1223506c0e87496d143c71199a2f7eeed8bd3213
Hamza Javed <hamzakhan.hk1994@gmail.com>
2024-11-09 19:15:39 +0500

    updated

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import '../style/Hero.css'; // Import the Hero.css file
 
+
 const Hero: React.FC = () => {
   return (
     <div id="hero" className="hero-container">
-       {/* Image element only visible on small screens */}
        <img className="profile-image" src="/profile2.jpeg" alt="Profile" />
       <div className="hero-content">
         <div className="spacer"></div>
